926a1670933c89fdfdb1cba918e4f34deee650c9
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-23 09:25:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const APP_ID = "0e9797a8";
+  const APP_KEY = "0724a07225075007c2a1b68e7701cd4e";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
       <h1>Hello React</h1>
