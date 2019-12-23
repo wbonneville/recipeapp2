df95af91a64499819d93c9a79dfc8a6176364a5e
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-23 09:46:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  // get id and key from api
   const APP_ID = "0e9797a8";
   const APP_KEY = "0724a07225075007c2a1b68e7701cd4e";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [counter, setCounter] = useState(0);
+  // state hook
+  const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {});
+  // call get recipes function when the browser loads
+  useEffect(() => {
+    getRecipes();
+  });
+
+  // fetch recipes from api
+  const getRecipes = async () => {
+    // wait for information to load
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    // set data from the response and turn into into a JSON object
+    const data = await response.json();
+    // set state of recipes = the array of recipes
+    // setRecipes(data.hits);
+  };
 
   return (
     <div className="App">
