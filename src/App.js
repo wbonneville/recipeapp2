92c7dc341bc98dc566d5c4e258d00e22389b35c2
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-23 09:33:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,9 +7,22 @@ const App = () => {
   const APP_KEY = "0724a07225075007c2a1b68e7701cd4e";
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {});
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form" action="">
+        <input className="search-bar" type="text" />
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="search-button"
+          type="submit"
+        >
+          {counter}
+        </button>
+      </form>
     </div>
   );
 };
