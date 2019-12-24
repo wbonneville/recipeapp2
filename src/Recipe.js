7c08d10f112ba21480eeb17f2db53831f2121ca4
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-24 11:26:21 -0500

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,21 @@
 import React from "react";
 import style from "./recipe.module.css";
 
+// destructuring
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
+    // style with CSS module
     <div className={style.recipe}>
       <h1>{title}</h1>
+      {/* map through ingredients */}
       <ol>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
+      {/* display calories */}
       <p>{calories}</p>
+      {/* style with CSS module */}
       <img className={style.image} src={image} alt="" />
     </div>
   );
